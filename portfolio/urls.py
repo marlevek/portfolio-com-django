a976c5730caa0c3139809271ef70391642862823
marlevek <marcelo_levek@hotmail.com>
2025-07-01 14:43:16 -0300

    adicionada nova rota

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('lista_projetos/', views.lista_projetos, name='lista_projetos'),
+    path('projeto/<str:id_projeto>/', views.detalhes_projeto, name='detalhes_projeto'),
 ]
