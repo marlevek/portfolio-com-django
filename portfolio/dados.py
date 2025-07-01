0c80c10f9315d45aa117e7e231c7d41b7d20e2c0
marlevek <marcelo_levek@hotmail.com>
2025-07-01 13:47:53 -0300

    pasta do aplicativo

---
+++ portfolio/dados.py
@@ -0,0 +1,17 @@
+habilidades = {
+    'habilidade1': {
+      'nome': 'Python'
+    },
+    'habilidade2':{
+      'nome': 'Django'
+      },
+    'habilidade3':{
+      'nome': 'Ciência de Dados'
+      },
+    'habilidade4':{
+      'nome': 'Machine Learning e IA'
+      },
+    'habilidade5':{
+      'nome': 'Git e GitHub'
+      },
+}
---
+++ portfolio/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .dados import habilidades
+
+
+def home(request):
+    return render(request, 'home.html', {'habilidades': habilidades})
