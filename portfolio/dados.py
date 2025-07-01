c59eee69e0d8942bc30f13d4ea0757bd184e68f3
marlevek <marcelo_levek@hotmail.com>
2025-07-01 14:37:35 -0300

    dicionário de dados

--- portfolio/dados.py
+++ portfolio/dados.py
@@ -15,3 +15,27 @@ habilidades = {
       'nome': 'Git e GitHub'
       },
 }
+
+projetos = {
+  'projeto1': {
+    'titulo': 'Blog',
+    'descricao': 'Primeiro projeto desenvolvido com Django em 2022',
+    'tecnologia': 'Python, Django, Git, GitHub',
+    'github': 'link_blog',
+    },
+  
+  'projeto2': {
+    'titulo': 'Gerador de conteúdo para redes sociais',
+    'descricao': 'Aplicativo para gerar conteúdo para redes sociais',
+    'tecnologia': 'Python, Streamlit,  Groq API, LangChain, Git, GitHub',
+    'github': 'https://github.com/marlevek/app_marketing',
+    },
+  
+  'projeto3': {
+    'titulo': 'Dashboard Inteligente',
+    'descricao': 'Apistema inteligente de monitoramento de ar-condicionado com foco em manutenção preditiva, geração automática de relatórios PMOC e visualização de riscos via dashboard.',
+    'tecnologia': 'Python, Streamlit, Pandas, Matplotlib, FPDF, Git, GitHub',
+    'github': 'https://github.com/marlevek/app_smartcool_pmoc',
+    },
+    
+}
