d136f0a9108952bd24e56d939ad560c82d3b4a8f
marlevek <marcelo_levek@hotmail.com>
2025-07-19 11:38:41 -0300

    modificadas as views

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
-from .dados import habilidades, projetos
+from .models import Habilidade, Projeto
 
 
 def home(request):
+    habilidades = Habilidade.objects.all()
     return render(request, 'home.html', {'habilidades': habilidades})
 
 
 def lista_projetos(request):
+    projetos = Projeto.objects.all()
     return render(request, 'projetos.html', {'projetos': projetos})
 
 
 def detalhes_projeto(request, id_projeto):
-    projeto = projetos.get(id_projeto)
+    projeto = Projeto.objects.get(id=id_projeto)
     return render(request, 'detalhes_projeto.html', {'projeto': projeto})
