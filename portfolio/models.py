1146b8f8524bf4247178c2a9835fdad34d11ffca
marlevek <marcelo_levek@hotmail.com>
2025-07-19 11:36:39 -0300

    modificado o models.py

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Habilidade(models.Model):
+    nome = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nome 
+
+    
+class Projeto(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    tecnologia = models.CharField(max_length=150)
+    github = models.URLField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.titulo 
